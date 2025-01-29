90499beb6e3aa0a7883fccfeaa01b8b5ef0c7896
Mohammed Asif Iqbal <asifintemb@gmail.com>
2025-01-29 15:50:27 -0500

    Initial commit

--- Frontend/src/pages/Design.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-
-const Design= () => {
-    return <h1>Design Page</h1>
-};
-export default Design;
---
+++ Frontend/src/pages/temp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../components/style.css';
+
+const Design= () => {
+    return <div>
+        <div className="design_container">
+        
+
+        </div>
+
+    </div>
+};
+export default Design;
